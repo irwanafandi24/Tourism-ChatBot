2bf8d3b09f6b859ac771e6136b41d74d7b19f437
irwanafandi24 <mohamadirwanafandi@gmail.com>
2021-03-18 23:24:49 +0700

    chatbot response design

--- Responses.py
+++ Responses.py
@@ -1,15 +1,24 @@
 import re
-name, location, destination, member, days = '', '', '', 0, 0
+name, location, destination, member, days, current_location = '', '', '', 0, 0, ''
 
 def message_response(input_text):
-    global name, location, destination, member, days
+    global name, location, destination, member, days, current_location
 
     user_message = str(input_text).lower()
     message_split = re.split("[' ]", user_message)
 
-    if user_message.startswith("i am") or user_message.startswith("i'm"):
-        name = (' '.join(message_split[2:])).capitalize()
-        reply_text = "Hai "+name+"! Nice to meet you.\nWhere are you know? (<b><i>In</i></b> ... )"
+    ##get destination
+    if user_message == 'bali':
+        destination = 'Bali'
+
+    if user_message.startswith("saya"):
+        name = (' '.join(message_split[1:])).capitalize()
+        reply_text = "Hai "+name+"! salam kenal ya.\nSudah tahu mau liburan kemana? Chavel punya rekomendasi nih special buat kamu, <b><i>Mungkin kesini asik</i></b>"
+        return reply_text
+
+    if user_message.endswith("hari"):
+        days = int(user_message.split()[0])
+        reply_text = "Biar <b>"+str(days)+" harimu</b> tidak terbuang sia-sia, ayo kita buat planning yang matang. Biasanya orang yang ke "+destination+" itu juga ke:"
         return reply_text
 
     if user_message.startswith("in"):
@@ -22,12 +31,10 @@ def message_response(input_text):
         reply_text = "How many people will go to "+destination+"? ( ... <b><i>people</i></b>)"
         return reply_text
 
-    if user_message.endswith("people"):
-        member = int(user_message.split()[0])
-        reply_text = "How many days will you stay in "+destination+"? ( ... <b><i>days</i></b>)"
-        return reply_text
-
     if user_message.endswith("days"):
         days = int(user_message.split()[0])
         reply_text = "Ok <b><i>"+name+"</i></b>, This is what <b>Cony</b> gets from the data that you have entered.\n\nCurrent Location\t: "+location+"\nDestination\t: "+destination+"\nStayed For\t: "+str(days)+" days\nNumber of members\t: "+str(member)+"\n\nCony will find the best recommendation for you:) <b>Wait a minute...</b>"
+        return reply_text
+    else:
+        reply_text = "I'm Sorry, I don't know what you mean "+name+" :cry:\nYou can click this word <b><i>/help</i></b>."
         return reply_text
--- main.py
+++ main.py
@@ -1,34 +1,105 @@
 from telegram import *
 from telegram.ext import *
+from telegram import ReplyKeyboardMarkup
 import wikipedia
 import requests
 import Constant as keys
 import Responses as R
 
+
 def start_command(update:Update, context:CallbackContext):
-    url = 'https://static.vecteezy.com/system/resources/thumbnails/000/242/789/small/jungle-explorers-vector-illustration.jpg'
+    url = 'https://cdn1.vectorstock.com/i/thumb-large/47/45/cartoon-comical-character-bali-kids-vector-34684745.jpg'
     context.bot.send_photo(chat_id=update.effective_chat.id, photo=url)
     update.message.reply_text(
-        text='Hai.. <b>welcome to Compas Tourism Bot</b>'
-             '\nMy name is Cony, your private guide.'
-             "\nI will guide you to get your perfect vocation spot. But, what is your name..? (<b><i>I am</i></b> or <b><i>I'm</i></b> ... )",
+        text="Hai..!! <b>welcome to ChavellBot </b>\
+                     \nPanggil saja kami <b><i>Chavel</i></b>, disini kami akan memandu anda untuk keliling Indonesia.\
+                     \nUpss sabar, tak kenal maka tak sayang. Yuk kenalan dulu, siapa namamu..? (<b><i>saya</i></b> ... )",
         parse_mode=ParseMode.HTML
     )
+
+def help_command(update:Update, context:CallbackContext):
+    update.message.reply_text(
+        text="<b><i>/start</i></b> : to start the bot\n<b><i>/vocation</i></b> : show all possible vocation\n<b><i>/food </i></b> : show all restourant near you",
+        parse_mode=ParseMode.HTML
+    )
+
 
 def handle_message(update:Update, context:CallbackContext):
     text = str(update.message.text).lower()
+    print(text)
     response = R.message_response(text)
-    update.message.reply_text(
-        text=response,
-        parse_mode=ParseMode.HTML
-    )
+    if text.startswith("saya"):
+        keyboard1 = [[
+            InlineKeyboardButton('Bali', callback_data='BALI'),
+            InlineKeyboardButton('Danau Toba', callback_data='DNTOBA'),
+            InlineKeyboardButton('Yogyakarta', callback_data='YGY'),
+            ],
+            [
+            InlineKeyboardButton('Labuan Bajo', callback_data='LABBJO'),
+            InlineKeyboardButton('Likupang', callback_data='LKP'),
+            InlineKeyboardButton('Mandalika', callback_data='MDL')
+         ]]
+        vocation_markup = InlineKeyboardMarkup(keyboard1, resize_keyboard=True)
+        context.bot.send_message(chat_id=update.effective_chat.id, text=response,
+                                 reply_markup=vocation_markup, parse_mode=ParseMode.HTML)
+    elif text.endswith("hari"):
+        menu_keyboard = [['Pantai Sanur'], ['Bedugul'], ['Pantai Kuta']]
+        menu_markup = ReplyKeyboardMarkup(menu_keyboard, one_time_keyboard=True, resize_keyboard=True)
+        context.bot.send_message(chat_id=update.effective_chat.id, text=response,
+                                 reply_markup=menu_markup, parse_mode=ParseMode.HTML)
+
+    else:
+        response = R.message_response(text)
+        update.message.reply_text(
+            text=response,
+            parse_mode=ParseMode.HTML
+        )
+
+    # if text.startswith("in"):
+    #     # holiday_url = 'https://commonslibrary.parliament.uk/content/uploads/2019/03/holiday-2103171_1920-1860x1280.jpg'
+    #     # keyboard1 = [[
+    #     #     InlineKeyboardButton('Bali', callback_data='BALI'),
+    #     #     InlineKeyboardButton('Danau Toba', callback_data='DNTOBA'),
+    #     #     InlineKeyboardButton('Yogyakarta', callback_data='YGY'),
+    #     #     ],
+    #     #     [
+    #     #     InlineKeyboardButton('Labuan Bajo', callback_data='LABBJO'),
+    #     #     InlineKeyboardButton('Likupang', callback_data='LKP'),
+    #     #     InlineKeyboardButton('Mandalika', callback_data='MDL')
+    #     # ]]
+    #     #
+    #     # reply_markup1 = InlineKeyboardMarkup(keyboard1, resize_keyboard=True)
+    #     # context.bot.send_photo(chat_id=update.effective_chat.id, photo=holiday_url, caption="Where do you want to go?", reply_markup=reply_markup1)
+    #
+    #     menu_keyboard = [['MenuItem1'], ['MenuItem2']]
+    #     menu_markup = ReplyKeyboardMarkup(menu_keyboard, one_time_keyboard=True, resize_keyboard=True)
+    #
+    #     context.bot.send_message(chat_id=update.effective_chat.id, text="Where do you want to go?", reply_markup=menu_markup)
+    #
+    # else:
+    #     response = R.message_response(text)
+    #     update.message.reply_text(
+    #         text=response,
+    #         parse_mode=ParseMode.HTML
+    #     )
+
+def button_click(update:Update, context:CallbackContext):
+    query: CallbackQuery = update.callback_query
+
+    if query.data == 'BALI':
+        response = R.message_response('Bali')
+        url_bali = 'https://www.korinatour.co.id/wp-content/uploads/2018/12/6-300x300.png'
+        text_bali = "Saya suka dengan selera anda :D\
+                    \n\nBerkunjung ke <b>Pulaunya para Dewa</b> memang memberikan kesan tersendiri.Kamu akan dimajakan dengan keindahan alam, kuliner dan juga adat istiadatnya. Gak sabar kan? Sabar ya Chavel mau tanya nih, berapa hari kamu akan liburan di Bali? (... <b><i>hari</i></b>)"
+        context.bot.send_photo(chat_id=update.effective_chat.id, photo=url_bali, caption=text_bali, parse_mode=ParseMode.HTML)
 
 def error(update:Update, context:CallbackContext):
     update.message.reply_text(
-        text='Sorry, Cony Does not understand what you are saying.'
-             '\nType <b><i>\help</i></b> and Cony will help you.',
+        text="Sorry, Cony Does not understand what you are saying.\
+             \nType <b><i>\help</i></b> and Cony will help you.",
         parse_mode=ParseMode.HTML
     )
+
 
 def main():
     print('bot started...')
@@ -36,8 +107,10 @@ def main():
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start_command))
+    dp.add_handler(CommandHandler("help", help_command))
 
     dp.add_handler(MessageHandler(Filters.text, handle_message))
+    dp.add_handler(CallbackQueryHandler(button_click))
 
     dp.add_error_handler(error)
 
